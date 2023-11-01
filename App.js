5de60dda381c1a244d19ed0101232523c705e8fb
ashutosh kushwah <ashutoshkushwah2014@gmail.com>
2023-11-01 11:44:32 +0530

    h1 tag created

---
+++ App.js
@@ -0,0 +1,4 @@
+const h1=React.createElement("h1",{"xyz-attr":"pta hai na",id:"heading"},"Hello from react!");
+console.log(h1);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
