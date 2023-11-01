65c64915aff0c91606bd66842c0de5cf99b49811
ashutosh kushwah <ashutoshkushwah2014@gmail.com>
2023-11-01 11:48:58 +0530

    single child

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const h1=React.createElement("h1",{"xyz-attr":"pta hai na",id:"heading"},"Hello from react!");
+const h1 = React.createElement("h1", { id: "heading" }, "Hello from react!");
+const div = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, h1))
+
 console.log(h1);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(h1);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
