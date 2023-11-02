7ab1d9e2be89516b04200f6b82e96fd07254a1f4
ashudamn <ashutoshkushwah2014@gmail.com>
2023-11-02 12:02:20 +0530

    added parcel

---
+++ Episode-2/App.js
@@ -0,0 +1,4 @@
+const heading=React.createElement("h1",{id:"heading"},"hi from react scripts");
+const root=ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render(heading);
---
+++ Episode-2/dist/index.6bd02f5a.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "hi from react scripts");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
