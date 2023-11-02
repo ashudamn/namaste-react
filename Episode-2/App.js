45f9e3aa1d32315d1e90ae41d8ee34f7b9e201a7
ashudamn <ashutoshkushwah2014@gmail.com>
2023-11-02 12:20:11 +0530

    removed scriopt tag

--- Episode-2/App.js
+++ Episode-2/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
 const heading=React.createElement("h1",{id:"heading"},"hi from react scripts");
 const root=ReactDOM.createRoot(document.getElementById("root")); 
 
